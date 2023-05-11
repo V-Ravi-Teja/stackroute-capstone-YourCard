ff0465be3466dfc6b57f52cc80623b08de1c4336
V-Ravi-Teja <64315770+V-Ravi-Teja@users.noreply.github.com>
2023-05-11 09:36:33 +0530

    initial commit

---
+++ YourCard/src/main/java/com/ness/YourCard/YourCardApplication.java
@@ -0,0 +1,13 @@
+package com.ness.YourCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourCardApplication.class, args);
+	}
+
+}
---
+++ YourCard/src/test/java/com/ness/YourCard/YourCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ness.YourCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
